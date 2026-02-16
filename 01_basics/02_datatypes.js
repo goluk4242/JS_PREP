6b5a65d4fa2ec24f27f907bfd2c1937d33f7fa40
goluk4242 <goluk4242@gmail.com>
2026-02-16 19:25:32 +0000

    datatypes discuss

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node js not browser
+
+let name = "lakshay" //string
+let age = 18 //number 
+let isLogged = false //boolean
+
+// number => 2 to power 53
+// bigint 
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof "lakshay")
+console.log(typeof null) //object
+console.log(typeof undefined) // undefined
